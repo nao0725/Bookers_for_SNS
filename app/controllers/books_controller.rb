8cf9d6817ed4a75db4d84fd39d2e34d97ea30587
nao0725 <aqua.725.times@gmail.com>
2021-07-07 12:33:28 +0000

    Update sort past 1weeks

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,8 +17,10 @@ class BooksController < ApplicationController
   end
 
   def index
+   to  = Time.current.at_end_of_day
+   from  = (to - 6.day).at_beginning_of_day
    @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size }
-  # @books = Book.all
+   # @books = Book.all
    @user = current_user
    @book = Book.new
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,10 +6,7 @@ class Book < ApplicationRecord
   has_many :favorited_users, through: :favorites, source: :user #いいねソート順にするため追加
   has_many :book_comments, dependent: :destroy
 
-  # 並び替えするメソッド
-  from  = Time.current.at_beginning_of_day
-  to    = (from + 6.day).at_end_of_day
-  books = Book.where(created_at: from...to)
+
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
