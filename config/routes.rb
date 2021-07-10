94c88d08208b0f831db8f9d6a3310fc2b7250ad7
nao0725 <aqua.725.times@gmail.com>
2021-07-10 08:11:08 +0000

    Update error

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
+  #トップページ
   root to: "homes#top"
   get "home/about" => "homes#about"
 
+  #User
   devise_for :users
   resources :users, only: [:index,:show,:edit,:update]
 
+  #book,コメント、いいね
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
-  resources :relationships, only: [:create, :destroy] 
+
+  #フォロー、フォロワー機能
+  resources :relationships, only: [:create, :destroy]
    get "relationship/follow_users" => "relationships#follow_users"
    get "relationship/follower_users" => "relationships#follower_users"
-   
+
+  #検索機能
   get "/search" => "searches#search"
 
+  #DM機能
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create, :show, :index]
 end
