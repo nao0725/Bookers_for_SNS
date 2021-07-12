204c7a6e8233468ecb6c117a5b56adbf895602e4
nao0725 <aqua.725.times@gmail.com>
2021-07-12 11:47:59 +0000

    Add to room_id in Message table

---
+++ db/migrate/20210712114323_add_room_id_t_to_message.rb
@@ -0,0 +1,5 @@
+class AddRoomIdTToMessage < ActiveRecord::Migration[5.2]
+  def change
+    add_column :messages, :room_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_121846) do
+ActiveRecord::Schema.define(version: 2021_07_12_114323) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_07_10_121846) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
